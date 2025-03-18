b672dc9eb4fb06e74e1815f3c5d4fde24c83281c
Chris Reynolds <chris@jazzsequence.com>
2025-03-18 13:43:21 -0600

    add artbox theme

---
+++ web/app/themes/artbox/footer.php
@@ -0,0 +1,14 @@
+<!-- begin footer -->
+	</div>
+
+	<?php get_sidebar(); ?>
+
+	<p class="credit">
+		<cite>
+			<?php sprintf( _e('the kidsblog is powered by <a href="%s"><strong>WordPress</strong></a>' ), 'https://wordpress.org' ); ?>
+		</cite>
+	</p>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
